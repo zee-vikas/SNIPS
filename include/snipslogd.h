eed7ef9f4eca5989416f5acbb74ca5d82afb3046
vikas <>
2001-07-08 22:04:27 +0000

    For SNIPS v1.0

---
+++ include/snipslogd.h
@@ -0,0 +1,33 @@
+/* $Header$ */
+
+
+#ifndef __snipslogd_h
+# define __snipslogd_h
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "snips.h"
+
+/* For extracting the snipslog port from /etc/services */
+#ifndef SNIPSLOG_SERVICE
+# define SNIPSLOG_SERVICE	"snipslog"
+# define NLOG_SERVICE		"noclog"	/* nocol compatibility */
+#endif
+
+/* Port in case not available from /etc/services */
+#ifndef SNIPSLOG_PORT
+# define SNIPSLOG_PORT		5354
+#endif
+
+#define READ_TIMEOUT		5	/* secs before socket read timeout */
+
+#endif  /* !__snipslogd_h  */
